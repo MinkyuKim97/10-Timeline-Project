593f5627ca382e9524424e0ff413a3bf8072723d
MinkyuKim97 <kimm707@newschool.edu>
2025-12-15 23:15:20 -0500

    1

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
-import react from '@vitejs/plugin-react'
+// import react from '@vitejs/plugin-react'
 
-export default {
-  base: process.env.REPO_NAME,
-  plugins: [
-    react(),
-  ],
-}
+// export default {
+//   base: process.env.REPO_NAME,
+//   plugins: [
+//     react(),
+//   ],
+// }
+// vite.config.js
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  base: "/10-Timeline-Project/",
+  plugins: [react()],
+});
